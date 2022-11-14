11a1e983adc527339ec22a3f532d4ff59fad252f
Lorenzoscajola <117931214+Lorenzoscajola@users.noreply.github.com>
2022-11-14 17:53:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
-using namespace std;
+using namespace std; 
 
-int main() {
-   return 0;
+int main () {
+	float a;
+	float b;
+	float x;
+	float y;
+	
+	cin>> a;
+	cin>> b;
+	cin>> x;
+	cin>> y;
+	
+	if (x<0 and y>0){
+		cout<<(a*x-b*y)<<endl;
+	}else if (x>=0 and y<=0) {
+		cout<<(a*x*x-b*y)<<endl;	
+	}else{
+		cout <<(a*x+b*y*y)<<endl;
+	}
+    return 0;
 }
